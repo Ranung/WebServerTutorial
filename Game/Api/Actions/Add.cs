d30c27034086578294dcd4850de8a5ebc659ad7c
geonhui <geonhui@loadcomplete.com>
2023-02-03 10:41:10 +0900

    TodoApi 폴더 위치 변경, Game 솔루션 생성 (미들웨어 스터디)

---
+++ Game/Api/Actions/Add.cs
@@ -0,0 +1,17 @@
+﻿using Newtonsoft.Json.Linq;
+
+namespace Api.Actions;
+
+public class Add : IApiService
+{
+    public Task<JObject> ProcessAsync(JObject param)
+    {
+        var a = param.Value<int>("a");
+        var b = param.Value<int>("b");
+
+        return Task.FromResult(new JObject
+        {
+            ["sum"] = a + b
+        });
+    }
+}
---
+++ Game/Api/Actions/Echo.cs
@@ -0,0 +1,16 @@
+﻿using Newtonsoft.Json.Linq;
+
+namespace Api.Actions;
+
+public class Echo : IApiService
+{
+    public Task<JObject> ProcessAsync(JObject param)
+    {
+        var text = param.Value<string>("text");
+
+        return Task.FromResult(new JObject
+        {
+            ["text"] = text
+        });
+    }
+}
